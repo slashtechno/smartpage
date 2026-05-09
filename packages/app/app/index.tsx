c206f5d69f651245c6e2031ba982b18057570255
slashtechno <77907286+slashtechno@users.noreply.github.com>
2026-05-09 00:26:15 -0500

    add camera view

--- packages/app/app/index.tsx
+++ packages/app/app/index.tsx
@@ -1,27 +1,81 @@
-import { useTheme } from '@/src/theme';
-import { Text, View, StyleSheet } from 'react-native';
-let theme;
-
+import { useTheme } from "@/src/theme";
+import { Text, View, StyleSheet, Button, Platform, Dimensions
+} from "react-native";
+import { CameraView, useCameraPermissions } from "expo-camera";
 export default function Index() {
-  theme = useTheme();
+  let theme = useTheme();
   const styles = StyleSheet.create({
     container: {
       flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center',
-      backgroundColor: theme.background
+      backgroundColor: theme.background,
     },
     text: {
-      color: theme.text
-    }
+      color: theme.text,
+      textAlign: 'center'
+    },
 
-  })
+    message: {
+      textAlign: "center",
+      paddingBottom: 10,
+    },
+
+    cameraContainer: {
+      // aspectRatio: 4 / 3,
+      overflow: "hidden", // clip to aspect ratio
+      // aspectRatio: 3/4,
+      aspectRatio: Platform.select({ web: 4 / 3, default: 3 / 4 }),
+      width: "100%",
+
+      // controls itself within parent
+      alignSelf: "center",
+
+    },
+    cameraView: {
+      width: '100%',
+      height: '100%',
+    },
+    buttonContainer: {
+      position: "absolute",
+      bottom: 64,
+      flexDirection: "row",
+      backgroundColor: "transparent",
+      width: "100%",
+      paddingHorizontal: 64,
+    },
+    button: {
+      flex: 1,
+      alignItems: "center",
+    },
+  });
+
+  const [permission, requestPermission] = useCameraPermissions();
+  if (!permission) {
+    return <View />; // eventually change this to a loading spinner componnt
+  }
 
   return (
-    <View
-      style={styles.container}
-    >
+    <View style={styles.container}>
       <Text style={styles.text}>Home Screen</Text>
+      {/*Expand this container in whatever space is left after the text (using flex) and then center children on the primary axis*/}
+      <View style={{justifyContent: "center", flex: 1}}>
+      {permission.granted ? (
+        <View style={styles.cameraContainer}>
+          <CameraView style={styles.cameraView} facing="back" />
+          {/*<View style={styles.buttonContainer}>
+            <TouchableOpacity style={styles.button} onPress={toggleCameraFacing}>
+              <Text style={styles.text}>Flip Camera</Text>
+            </TouchableOpacity>
+          </View>*/}
+        </View>
+      ) : (
+        <View style={styles.container}>
+          <Text style={styles.message}>
+            We need your permission to show the camera
+          </Text>
+          <Button onPress={requestPermission} title="grant permission" />
+        </View>
+        )
+        }</View>
     </View>
   );
 }
