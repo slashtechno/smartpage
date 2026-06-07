7e90d4ce768ebf1af41661ecdeb5e81361c8b78b
slashtechno <77907286+slashtechno@users.noreply.github.com>
2026-06-06 23:20:29 -0400

    specify workspace node_modules in Metro config

--- packages/app/metro.config.js
+++ packages/app/metro.config.js
@@ -1,8 +1,21 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
+const path = require("path");
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativewind } = require("nativewind/metro");
 
+const projectRoot = __dirname;
+const workspaceRoot = path.resolve(projectRoot, "../..");
+
 /** @type {import('expo/metro-config').MetroConfig} */
-const config = getDefaultConfig(__dirname);
+const config = getDefaultConfig(projectRoot);
+
+// In CI, Bun hoists workspace packages to the root node_modules rather than
+// installing them in packages/app/node_modules. Adding workspaceRoot here lets
+// Metro find packages in either location.
+config.watchFolders = [workspaceRoot];
+config.resolver.nodeModulesPaths = [
+  path.resolve(projectRoot, "node_modules"),
+  path.resolve(workspaceRoot, "node_modules"),
+];
 
 module.exports = withNativewind(config);
